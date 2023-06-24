2f67c3f7ef708bfc7bc2a95c15b0c5f22e29d725
Gene Mueller <geneemueller@gmail.com>
2023-06-23 22:04:21 -0700

    forgot the button

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func euro_switch(_ sender: UISwitch) {
     }
+    @IBAction func yen_switch(_ sender: UISwitch) {
+    }
+    @IBAction func rupee_switch(_ sender: UISwitch) {
+    }
+    @IBAction func ruble_switch(_ sender: UISwitch) {
+    }
+    
+    
+    
 }
 
