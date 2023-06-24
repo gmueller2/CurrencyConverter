d342a91b2bfc7e1fabca07af56eb01e7db2943b9
Gene Mueller <geneemueller@gmail.com>
2023-06-23 20:23:53 -0700

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Gene Mueller on 6/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
